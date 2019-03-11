9e88ddddf4187c9d28153a6846eba9aac7b3602d
Aszmel <aszmel@o2.pl>
2019-03-11 12:58:41 +0100

    workin on Updating Item, added UpdateItem.js and update.js in pages

--- sick-fits/backend/src/resolvers/Mutations.js
+++ sick-fits/backend/src/resolvers/Mutations.js
@@ -13,13 +13,23 @@ const Mutation = {
     );
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of the updates
+    const updates = { ...args };
+    //remove the ID from the updates
+    delete updates.id;
+    //run the update methed
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
 };
 
 module.exports = Mutation;
