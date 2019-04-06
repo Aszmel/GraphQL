744f06e581b7e47cedb839089d348ab86c325714
Aszmel <aszmel@o2.pl>
2019-04-06 19:30:58 +0100

    added Permission page&component to display users and their permissions

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,10 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
     // check if there is a current user ID
     if (!ctx.request.userId) {
@@ -11,11 +12,21 @@ const Query = {
     }
     return ctx.db.query.user(
       {
-        where: { id: ctx.request.userId },
+        where: { id: ctx.request.userId }
       },
       info
     );
   },
+  async users(parent, args, ctx, info) {
+    //check if they're logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    //check if user has permission to query all users
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+    //if user has permission, query all users
+    return ctx.db.query.users({}, info);
+  }
 };
 
 module.exports = Query;
