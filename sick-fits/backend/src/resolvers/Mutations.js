dd27ff0f6250398e1cd82c4a05e081f89ef569eb
Aszmel <aszmel@o2.pl>
2019-04-07 16:22:13 +0100

    updating permissions on the server

--- sick-fits/backend/src/resolvers/Mutations.js
+++ sick-fits/backend/src/resolvers/Mutations.js
@@ -4,6 +4,7 @@ const { randomBytes } = require("crypto");
 const { promisify } = require("util");
 //promisify is in node util lib that make Fx with callB a promise
 const { transport, makeANiceEmail } = require("../mail");
+const { hasPermission } = require("../utils");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -166,6 +167,37 @@ const Mutations = {
     });
     // 8. return the new user
     return updatedUser;
+  },
+  async updatePermissions(parent, args, ctx, info) {
+    //check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged to do that!");
+    }
+    //query the current user
+    const currentUser = await ctx.db.query.user(
+      {
+        where: {
+          id: ctx.request.userId
+        }
+      },
+      info
+    );
+    //check if they have permission to do that
+    hasPermission(currentUser, ["ADMIN", "PERMISSIONUPDATE"]);
+    //update the permissions
+    return ctx.db.mutation.updateUser(
+      {
+        data: {
+          permissions: {
+            set: args.permissions
+          }
+        },
+        where: {
+          id: args.userId
+        }
+      },
+      info
+    );
   }
 };
 
