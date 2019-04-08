3fe2b3493f3ec127dbd9490847a5c31aae667bd5
Aszmel <aszmel@o2.pl>
2019-04-08 20:27:25 +0100

    start displaying stuff in Cart

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import User from "./User";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
@@ -19,28 +20,38 @@ const TOGGLE_CART_MUTATION = gql`
 `;
 
 const Cart = () => (
-  <Mutation mutation={TOGGLE_CART_MUTATION}>
-    {toggleCart => (
-      <Query query={LOCAL_STATE_QUERY}>
-        {({ data }) => (
-          <CartStyles open={data.cartOpen}>
-            <haeder>
-              <CloseButton onClick={toggleCart} title="close">
-                &times;
-              </CloseButton>
-              <Supreme>Your Cart</Supreme>
-              <p>You have __ Items in your Cart</p>
-            </haeder>
+  <User>
+    {({ data: { me } }) => {
+      if (!me) return null;
+      return (
+        <Mutation mutation={TOGGLE_CART_MUTATION}>
+          {toggleCart => (
+            <Query query={LOCAL_STATE_QUERY}>
+              {({ data }) => (
+                <CartStyles open={data.cartOpen}>
+                  <haeder>
+                    <CloseButton onClick={toggleCart} title="close">
+                      &times;
+                    </CloseButton>
+                    <Supreme>{me.name} Cart</Supreme>
+                    <p>
+                      You have {me.cart.length} Item
+                      {me.cart.length === 1 ? "" : "s"} in your Cart
+                    </p>
+                  </haeder>
 
-            <footer>
-              <p>$10.10</p>
-              <SickButton>Checkout</SickButton>
-            </footer>
-          </CartStyles>
-        )}
-      </Query>
-    )}
-  </Mutation>
+                  <footer>
+                    <p>$10.10</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              )}
+            </Query>
+          )}
+        </Mutation>
+      );
+    }}
+  </User>
 );
 
 export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
