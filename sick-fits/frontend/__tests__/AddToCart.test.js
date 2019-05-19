8aa13adfedc70ed0111fafc40636788646f962d0
Aszmel <aszmel@o2.pl>
2019-05-19 20:19:08 +0100

    testing AddToCart & RemoveFromCart

--- sick-fits/frontend/__tests__/AddToCart.test.js
+++ sick-fits/frontend/__tests__/AddToCart.test.js
@@ -86,4 +86,17 @@ describe("<AddToCart/>", () => {
     expect(me2.cart[0].id).toBe("omg123");
     expect(me2.cart[0].quantity).toBe(3);
   });
+
+  it("changes from add to adding when clicked", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id="abc123" />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain("Add To Cart");
+    wrapper.find("button").simulate("click");
+    expect(wrapper.text()).toContain("Adding To Cart");
+  });
 });
